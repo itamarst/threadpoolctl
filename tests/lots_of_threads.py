ba8d196b1a56343285f85758a4354e13a3979876
Itamar Turner-Trauring <itamar@itamarst.org>
2026-07-13 14:09:11 -0400

    Make it more useful for CI

--- tests/lots_of_threads.py
+++ tests/lots_of_threads.py
@@ -1,6 +1,5 @@
-import sys
 from concurrent.futures import ThreadPoolExecutor
-from json import dumps
+from pprint import pprint
 from threading import Thread
 from typing import Callable
 
@@ -8,7 +7,11 @@ import numpy as np
 import threadpoolctl
 import psutil
 
-from tests._openmp_test_helper.openmp_helpers_inner import check_openmp_num_threads
+try:
+    from tests._openmp_test_helper.openmp_helpers_inner import check_openmp_num_threads
+except ImportError:
+    check_openmp_num_threads = None
+
 
 def start_counting_threads() -> Callable[[], int]:
     num_threads = 0
@@ -36,28 +39,27 @@ def blas():
     A = np.ones((10_000_000,))
 
     def blas_math(_):
-        for _ in range(5):
-            A.dot(A)
+        A.dot(A)
 
-    with ThreadPoolExecutor(10) as pool:
-        for _ in pool.map(blas_math, range(100)):
+    with ThreadPoolExecutor(100) as pool:
+        for _ in pool.map(blas_math, range(400)):
             pass
 
 
 def openmp():
+    if check_openmp_num_threads is None:
+        print("OpenMP not available")
+        return
+
     def run_openmp(_):
         check_openmp_num_threads(1000)
 
-    with ThreadPoolExecutor(10) as pool:
-        for _ in pool.map(run_openmp, range(100)):
+    with ThreadPoolExecutor(100) as pool:
+        for _ in pool.map(run_openmp, range(400)):
             pass
 
 
-
 def run(which: str) -> None:
-    print(threadpoolctl.threadpool_info())
-    threadpoolctl.threadpool_limits(12)
-
     if which == "blas":
         func = blas
     else:
@@ -66,8 +68,29 @@ def run(which: str) -> None:
     count_threads = start_counting_threads()
     func()
     max_num_threads = count_threads()
-    print(dumps({"max_threads": max_num_threads}))
+
+    # We're creating 100 Python threads, and asking for 2 native threads. If
+    # number is close to 100, we'll assume process-wide shared thread pool. If
+    # the number is close to 200, we'll assume a thread pool per thread.
+    if max_num_threads < 130:
+        scope = "process-wide shared thread pool"
+    elif max_num_threads > 170:
+        scope = "thread pool per thread"
+    else:
+        scope = "not sure"
+
+    print(f"== Observed behavior: {which} ==")
+    print("Maximum number of observed threads:", max_num_threads)
+    print("Presumed API scope:", scope)
+    print()
 
 
-if __name__ == '__main__':
-    run(sys.argv[1])
+if __name__ == "__main__":
+    threadpoolctl.threadpool_limits(2)
+    print("== Library info ==")
+    pprint(threadpoolctl.threadpool_info())
+    print()
+
+    run("openmp")
+    print()
+    run("blas")
