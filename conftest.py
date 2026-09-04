0452457b0759d0326abe72a232854ef3f9bfd44c
Itamar Turner-Trauring <itamar@itamarst.org>
2026-09-04 16:08:25 -0400

    Add another file to skip.

--- conftest.py
+++ conftest.py
@@ -1,1 +1,5 @@
-collect_ignore = ["tests/_openmp_test_helper", "tests/_limit_blas"]
+collect_ignore = [
+    "tests/empirical_scope_observation",
+    "tests/_openmp_test_helper",
+    "tests/_limit_blas",
+]
